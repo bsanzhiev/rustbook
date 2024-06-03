e3b9c0f73b2e599ca68d6457eb705d32f4834ea9
baatar <bsanzhiev@outlook.com>
2024-06-03 12:18:38 +0800

    Guessing game intro

---
+++ ch2-guessing-game/target/debug/ch2-guessing-game.d
@@ -0,0 +1,1 @@
+C:\Users\bash\devs\rustbook\ch2-guessing-game\target\debug\ch2-guessing-game.exe: C:\Users\bash\devs\rustbook\ch2-guessing-game\src\main.rs
---
+++ ch2-guessing-game/target/debug/deps/ch2_guessing_game.d
@@ -0,0 +1,5 @@
+C:\Users\bash\devs\rustbook\ch2-guessing-game\target\debug\deps\ch2_guessing_game.exe: src\main.rs
+
+C:\Users\bash\devs\rustbook\ch2-guessing-game\target\debug\deps\ch2_guessing_game.d: src\main.rs
+
+src\main.rs:
